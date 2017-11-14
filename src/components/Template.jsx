15008cca370c5faa61299b3519bf08127c8a0db9
Cody Chase <cchase1991@gmail.com>
2017-11-14 13:46:22 -0500

    broken

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from './shared/Header';
-import HomePage from './home/HomePage';
+import HomePage from './home/HomePageContainer';
+import LoginPage from './account/LoginPageContainer';
 import ProfilePage from './account/ProfilePage';
 
-export default function Template() {
+export default function Template(props) {
   return (
     <Router>
       <div className="wrapper">
         <Header username="anonymous" />
-        <Route exact path="/" component={HomePage} />
-        <Route path="/account/profile/:id" component={ProfilePage} />
+        <p>{props.progress}</p>
+        <section className="page-content container-fluid">
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/account/login" component={LoginPage} />
+          <Route path="/account/profile/:id" component={ProfilePage} />
+        </section>
       </div>
     </Router>
   );
